e1a26d9b1391ea209c2c464cbab7e172d6346760
michaelhuang <jinyanghuangjj@gmail.com>
2019-11-27 22:25:56 -0500

    fix

--- src/main/scala/ca/uwaterloo/cs451/a7/RegionEventCount.scala
+++ src/main/scala/ca/uwaterloo/cs451/a7/RegionEventCount.scala
@@ -33,6 +33,46 @@ import scala.collection.mutable
 object RegionEventCount {
   val log = Logger.getLogger(getClass().getName())
 
+  def insideGoldman (Double x, Double y){
+      val ax = -74.0141012
+      val ay = 40.7152191
+      val bx = -74.013777
+      val by = 40.7152275
+      val cx = -74.0141027
+      val cy = 40.7138745
+      val dx = -74.0144185
+      val dy = 40.7140753
+      val a = (bx - ax)*(y - ay) - (by - ay)*(x - ax);
+      val b = (cx - bx)*(y - by) - (cy - by)*(x - bx);
+      val c = (dx - cx)*(y - cy) - (dy - cy)*(x - cx);
+      val b = (ax - dx)*(y - dy) - (ay - dy)*(x - dx);
+      if((a>0 && b>0 && c>0 && d>0) || (a<0 && b<0 && c<0 && d<0)){
+          return true;
+      }else{
+          return false;
+      }
+  }
+
+  def insideCitigroup (Double x, Double y){
+      val ax = -74.011869
+      val ay = 40.7217236
+      val bx = -74.009867
+      val by = 40.721493
+      val cx = -74.010140
+      val cy = 40.720053
+      val dx = -74.012083
+      val dy = 40.720267
+      val a = (bx - ax)*(y - ay) - (by - ay)*(x - ax);
+      val b = (cx - bx)*(y - by) - (cy - by)*(x - bx);
+      val c = (dx - cx)*(y - cy) - (dy - cy)*(x - cx);
+      val b = (ax - dx)*(y - dy) - (ay - dy)*(x - dx);
+      if((a>0 && b>0 && c>0 && d>0) || (a<0 && b<0 && c<0 && d<0)){
+          return true;
+      }else{
+          return false;
+      }
+  }
+
   def main(argv: Array[String]): Unit = {
     val args = new EventCountConf(argv)
 
@@ -58,18 +98,18 @@ object RegionEventCount {
     val wc = stream.map(_.split(","))
       .map( p=> {
           if (p(0) == "yellow"){
-              if(p(10).toDouble <= -74.013777 && p(10).toDouble >= -74.0141012 && p(11).toDouble >= 40.7152191 && p(11).toDouble <= 40.7152275){
+              if (insideGoldman(p(10).toDouble, p(11).toDouble)){
                   ("goldman" , 1)
-              }else if(p(10).toDouble >= -74.011869 && p(10).toDouble <= -74.009867 && p(11).toDouble <= 40.7217236 && p(11).toDouble >= 40.721493){
+              }else if(insideCitigroup(p(10).toDouble, p(11).toDouble)){
                   ("citigroup",1)
               }else{
                   ("grabage",1)
               }
           }
           else{
-              if(p(8).toDouble <= -74.013777 && p(8).toDouble >= -74.0141012 && p(9).toDouble >= 40.7152191 && p(9).toDouble <= 40.7152275){
+              if(insideGoldman(p(8).toDouble, p(9).toDouble)){
                   ("goldman" , 1)
-              }else if(p(8).toDouble >= -74.011869 && p(8).toDouble <= -74.009867 && p(9).toDouble <= 40.7217236 && p(9).toDouble >= 40.721493){
+              }else if(insideCitigroup(p(8).toDouble, p(9).toDouble)){
                   ("citigroup",1)
               }else{
                   ("grabage",1)
